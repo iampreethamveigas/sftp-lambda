4b7b71039c9b105636f9c89457d32cb16c2f5e0d
iampreethamveigas <veigasbros@gmail.com>
2020-04-08 00:21:07 +0530

    config

---
+++ config/_config.db.js
@@ -0,0 +1,7 @@
+module.exports = {
+    user: process.env.db_user,
+    password: process.env.db_pass,
+    database: process.env.db_name,
+    host: process.env.db_host,
+    port: process.env.db_port
+};
---
+++ config/_config.sftp.js
@@ -0,0 +1,6 @@
+module.exports = {
+    host: process.env.sftp_host,
+    port: process.env.sftp_port,
+    username: process.env.sftp_username,
+    password: process.env.sftp_password
+};
